5cf9067a5445227c34e940144171c6802035c56a
Pablo Ramos <prm028@educa.madrid.org>
2026-02-05 04:20:57 +0100

    ver2

--- src/main/java/Empresa.java
+++ src/main/java/Empresa.java
@@ -2,14 +2,16 @@ public class Empresa {
     public static void main(String[] args) {
         Empleado e1= new Empleado("Juan", 33, 25000.6 );
         System.out.println(e1);
-        Departamento d1= new Departamento();
 
-        d1.setNombre("Informática");
+        /*Departamento d1= new Departamento();
+        d1.setNombre("Informática");*/
+
+        Departamento d1 = new Departamento("Informática");
         d1.setNumEmpleados(25);
 
         d1.setNumEmpleados(d1.getNumEmpleados()+1);
         d1.anadir(e1); System.out.println(d1); 
 
-        // Versión 1 verificada
+        // Versión 2 verificada
         } 
     }
